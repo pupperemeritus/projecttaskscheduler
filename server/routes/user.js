a6dc7dc9a1c3e3abe2983d4f3cac3f80f6f072b7
pupper emeritus <pupperemeritus9@protonmail.com>
2022-12-28 00:09:44 +0530

    edits for 27th

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,25 +2,25 @@
 const { response } = require('express');
 const express = require('express');
 const router = express.Router();
-const userModel = require('../models/userModel');
+const user = require('../models user');
 
 // Getting All Users
 router.get('/', async (req, res) => {
     try {
-        const users = await userModel.find();
+        const users = await user.find();
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 });
 
 // Getting One User
-router.get('/:id', getUser, (req, res) => {
-    res.send(res.user.name)
+router.get('/:id', async (req, res) => {
+    const getUser = await user.findById({ _id: id });
 });
 
 // Creating One User
 router.post('/', async (req, res) => {
-    const user = new userModel({
+    const user = new user({
         name: req.body.name,
         accountCreationDate: Date.now(),
         accountModificationDate: Date.now(),
@@ -35,32 +35,33 @@ router.post('/', async (req, res) => {
 });
 
 // Updating One User
-router.patch('/:id', (getUser, req, res) => {});
+router.patch('/:id', async (req, res) => {
+    const updatedUser = await user.where({ _id: id }).update({
+        $set: {
+            name: req.body.name,
+            email: req.body.email,
+            accountModificationDate: Date.now(),
+        },
+    });
+    try {
+        const upd = await user.save();
+        res.status(201).json(updatedUser);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
 
 // Deleting One User
-router.delete('/:id', (getUser, req, res) => {});
+router.delete('/:id', async (req, res) => {
+    try {
+        await user.deleteOne({ _id: id });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
 
-// Login
-
-//router.post();
+// router.post();
 
 // Register
-
-//router.post();
-
-// Middleware to process id
-const getUser = async (req, res, next) => {
-    let user;
-    try {
-        user = await user.findById(req.params.id);
-        if (user == null) {
-            return res.status(404).json({ message: 'Cannot find user' });
-        }
-    } catch (err) {
-        return res.status(500).json({ message: err.message });
-    }
-    res.user = user;
-    next();
-};
 
 module.exports = router;
