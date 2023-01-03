65e0b1f944c626131986a8297a798e8740c96982
pupper emeritus <pupperemeritus9@protonmail.com>
2023-01-03 19:15:15 +0530

    basic frontend structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,40 @@
 import React, { useEffect, useState } from 'react';
-
+import axios from 'axios';
 function App() {
-    const [backendData, setBackendData] = useState([{}]);
-    useEffect(() => {
-        fetch('/api')
-            .then((response) => response.json())
-            .then((data) => {
-                setBackendData(data);
-            });
-    });
+    // const [backendData, setBackendData] = useState(0);
+    // useEffect(() => {
+    //     fetch('/g')
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             setBackendData(data);
+    //         });
+    // });
+    <Routes>
+        <Route
+            path='/home'
+            element={<Home />}
+        />
+        <Route
+            path='/login'
+            element={<Login />}
+        />
+        <Route
+            path='/tasks'
+            element={<Tasks />}
+        />
+        <Route
+            path='/user'
+            element={<User />}
+        />
+        <Route
+            path='/groups'
+            element={<Group />}
+        />
+        <Route
+            path='/register'
+            element={<Register />}
+        />
+    </Routes>;
     return <div>App</div>;
 }
 
