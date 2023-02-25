7f93e5866a9316c893052d23efe5ee14b3c3f390
pupper emeritus <pupperemeritus9@protonmail.com>
2023-02-26 01:10:19 +0530

    minor fix

--- server/models/verifyJWT.js
+++ server/models/verifyJWT.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 function verifyJWT(req, res, next) {
     const token = req.headers['x-access-token']?.split(' ')[1];
     if (token) {
-        jwt.verify(token, process.env.PASSPORTSECRET, (err, decoded) => {
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
             if (err)
                 return res.json({
                     isLoggedIn: false,
